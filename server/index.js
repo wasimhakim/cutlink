b59727eac9a8645de76e870e39f22b669d5ac622
--add <waseem@perxtech.com>
2024-10-24 11:29:55 +0400

    FE Url list and form

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
-import express, { json } from 'express'
+import express, { json, urlencoded } from 'express'
 import { config } from 'dotenv'
+import cors from 'cors'
 
 import urlRoutes from './routes/urlRoutes.js'
+import { redirectUrl } from './controllers/urlController.js'
 
 config() // Load env variables
 
 const app = express()
+app.use(urlencoded({ extended: true }));
 app.use(json()) // Parse json requests
+app.use(cors())
 
 app.use('/url', urlRoutes);
+
+app.use('/*', redirectUrl)
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
--- server/routes/urlRoutes.js
+++ server/routes/urlRoutes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { shortenUrl, redirectUrl, urlList } from '../controllers/urlController.js';
+import { shortenUrl, urlList } from '../controllers/urlController.js';
 
 const router = Router();
 
 router.get('/', urlList);
 
 router.post('/shorten', shortenUrl);
-
-router.get('/*', redirectUrl)
 
 export default router;
